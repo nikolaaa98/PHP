3a04ba43be663c8623d3fd6cff7fac14dc8a45fc
nikolaaa98 <sdnikola72@gmail.com>
2021-09-02 14:43:40 +0200

    first commit

---
+++ site.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title></title>
+  </head>
+  <body>
+      <?php
+        echo "<p>This is my site</p>";
+        echo "<h1>Mike's ewasfdsSited</h1>";
+        echo "<hr";
+      ?>
+  </body>
+</html>
